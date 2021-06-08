48c6c77aae36858b6cfbfe10b85b653f4cc944f6
AhmadMaher1992 <ahmadeisa1992@gmail.com>
2021-06-08 11:41:56 +0200

    Starting Pagination

--- InstgramFirebase/UserProfile/UserProfileController.swift
+++ InstgramFirebase/UserProfile/UserProfileController.swift
@@ -20,7 +20,7 @@ class UserProfileController: UICollectionViewController {
     let  homePostCellId = "homePostCellId"
     var userID: String?
     var isGridView = true
-    
+    var isFinishedPaging = false
     
     //==========================================================
     // MARK:- App Life Cycle
@@ -51,35 +51,13 @@ class UserProfileController: UICollectionViewController {
     
     
     
-    fileprivate func fetchOrderPosts(){
-        
-       // guard let uid = Auth.auth().currentUser?.uid else { return }
-        guard let uid = user?.uid else { return }
-        let ref = Database.database().reference().child("posts").child(uid)
-        //create observer at this node
-     
-        ref.queryOrdered(byChild: "creationDate").observe(.childAdded, with:{ (snapshot) in
-       
-            guard let dictionary = snapshot.value as? [String: Any] else { return }
-            guard let user = self.user else { return}
-            let post = Post(user: user , dictionary: dictionary)
-            self.posts.insert(post, at: 0)
-            
-            self.collectionView?.reloadData()
-      
-        } ) {(error) in
-            print("Failed to fetch ordered Posts",error)
-        }
-        
-      
-        
-        
-    }
+
    
     
     fileprivate func setupLogOutButton(){
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "gear").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleLogout))
     }
+    
     @objc func handleLogout(){
         let alertController = UIAlertController(title: nil , message: nil, preferredStyle: .actionSheet)
         alertController.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (_) in
@@ -107,10 +85,75 @@ class UserProfileController: UICollectionViewController {
             self.user = user
             self.navigationItem.title = self.user?.username
             self.collectionView?.reloadData()
-            self.fetchOrderPosts()
+            self.paginatePosts()
         }
     }
     
+    fileprivate func paginatePosts(){
+        
+        print("DEBUG: Start Paginate Posts")
+        guard let uid = user?.uid else { return }
+        let ref = Database.database().reference().child("posts").child(uid)
+
+        var query = ref.queryOrderedByKey()
+        if posts.count > 0 {
+            let value = posts.last?.id
+           query = query.queryStarting(atValue: value)
+            
+        }
+        query.queryLimited(toFirst: 4).observeSingleEvent(of: .value) { (snapshot) in
+            
+            guard var allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
+            
+            if allObjects.count < 4 {
+                
+                self.isFinishedPaging = true
+            }
+            if self.posts.count > 0 {
+                allObjects.removeFirst()
+            }
+           
+            guard let user = self.user else { return }
+            allObjects.forEach({ (snapshot) in
+              //  print("DEBUG: Pagination Snap shot \(snapshot.key)")
+                guard let dictionary = snapshot.value as? [String: Any] else { return }
+                var post = Post(user: user, dictionary: dictionary)
+                post.id = snapshot.key
+                self.posts.append(post)
+                
+                
+            })
+            self.posts.forEach({ print($0.id ?? "")} )
+            self.collectionView.reloadData()
+            
+        } withCancel: { (error) in
+            print("DEBUG: Failed to fetch ordered Posts",error)
+        }
+
+        
+    }
+    
+    fileprivate func fetchOrderPosts(){
+        
+       // guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let uid = user?.uid else { return }
+        let ref = Database.database().reference().child("posts").child(uid)
+        //create observer at this node
+     
+        ref.queryOrdered(byChild: "creationDate").observe(.childAdded, with:{ (snapshot) in
+       
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            guard let user = self.user else { return}
+            let post = Post(user: user , dictionary: dictionary)
+            self.posts.insert(post, at: 0)
+            
+            self.collectionView?.reloadData()
+      
+        } ) {(error) in
+            print("Failed to fetch ordered Posts",error)
+        }
+        
+    }
     
     
     
@@ -142,6 +185,12 @@ extension UserProfileController {
     
     //Cell For Collection
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //if we reach the last cell and not finish paging continue processing of pagination
+        if indexPath.item == self.posts.count - 1 && !isFinishedPaging {
+            print("DEBUG: Cell For Item At PAGINATION")
+            self.paginatePosts()
+            
+        }
         
         if isGridView{
             
